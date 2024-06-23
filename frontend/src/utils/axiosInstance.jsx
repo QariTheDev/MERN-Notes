2e68f5c7997ef329ad10081fbaee678d3e8a68df
QariTheDev <talhaiqbal7272@gmail.com>
2024-06-23 14:42:11 +0500

    url

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://mern-notes-two-sigma.vercel.app/api/v1';
+const URL = 'https://mern-notes-backend-nu.vercel.app/';
 
 const axiosInstance = axios.create({
     baseURL: URL,
